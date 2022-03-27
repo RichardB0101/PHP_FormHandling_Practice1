ae10cc9f306a5aee6d9aebb06181a05a9d287419
Not_A_Junior <richard.bogu@gmail.com>
2022-03-27 22:15:26 +0300

    Gonna need refactor soon, lol

---
+++ Learning stuff/Projects/Portfolio/Task1/cont_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Content page</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+
+    <div class="wrapperLogin">
+    <p>0</p>
+
+      <button type="submit" >+1</button></br></br>
+      <button type="submit">Logout</button>
+    
+    
+    </div>
+
+  </body>
+</html>
+    
--- Learning stuff/Projects/Portfolio/Task1/index.php
+++ Learning stuff/Projects/Portfolio/Task1/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Main page</title>
     <link rel="stylesheet" href="styles.css">
   </head>
   <body>
@@ -27,11 +27,12 @@
     <button type="submit" >Register</button></br></br>
     
     </form>
-
-    <form action="register.php">
+    <form id="submitForm" action="cont_page.php">
         <button type="submit">Sign In</button>
     </form>
-
+  <?php 
+  #<p>0</p>
+  ?>
     </div>
   </body>
 </html>

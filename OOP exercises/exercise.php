f036231c7be01b36825494bd70461f1404479584
SundayCoder81 <richard.bogu@gmail.com>
2022-04-25 18:45:35 +0300

    Added some code

--- OOP exercises/exercise.php
+++ OOP exercises/exercise.php
@@ -2,4 +2,33 @@
 
 //OOP exercises
 
+class Character{
+    protected $name;
+    protected $class;
+    protected $level;
+
+
+    //creating constructor, to set names on object creation
+    function __construct($name, $class, $level){
+        $this->name = $name;
+        $this->class = $class;
+        $this->level = $level;  
+    }
+    
+    function getName(){
+        return $this->name;
+    }
+    function getClass(){
+        return $this->class;
+    }
+    function getLevel(){
+        return $this->level;
+    }
+
+
+    }
+    
+
 echo "<h2>PHP OOP exercises:</h2>";
+
+$lancelot = new Character("Lancelot642", "Knight", 1);
